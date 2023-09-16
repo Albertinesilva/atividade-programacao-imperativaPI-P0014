d56f801d2b727a470c7703ae389e00ead393f6e1
João Ramos <jvitorsb98@outlook.com>
2023-09-16 13:08:02 -0300

    novas funções

--- exercicio4.cpp
+++ exercicio4.cpp
@@ -25,6 +25,27 @@ void divisao(){
     cout << "Divisão entre "<< x << " e " << y << " : " << x/y;
 }
 
+void soma(){
+    float x,y;
+    cout << "Entre com o valor de do primeiro numero: " ;
+    cin >> x;
+    
+    cout << "Entre com o valor de do segundo numero: " ;
+    cin >> y;
+
+    cout << "Soma entre "<< x << " e " << y << " : " << x+y;
+}
+
+void subtração(){
+    float x,y;
+    cout << "Entre com o valor de do primeiro numero: " ;
+    cin >> x;
+    
+    cout << "Entre com o valor de do segundo numero: " ;
+    cin >> y;
+
+    cout << "Subtração entre "<< x << " e " << y << " : " << x-y;
+}
 
 int main(){
 
